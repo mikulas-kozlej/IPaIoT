8770967a475e432a2a02747042d88625731a9a89
mikulas-kozlej <mikulas.kozlej@student.tuke.sk>
2022-11-03 10:39:18 +0100

    vs code test

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php
 
-echo '<h1> Daco 1 </h1>';
+echo '<h1>Daco 1</h1>';
+echo '<h1>Daco 2</h2>';
+echo '<h2>zas daco</h2>';
+echo '<p>dufam ze uz funguje</p>'
 
-echo '<h1> Daco 2 </h2>';
 
-echo '<h2> zas daco </h2>';
 
 ?>

7c2489ff0c06ed45a053c44c0a9f93a1d574f17e
mikulas-kozlej <mikulas.kozlej@student.tuke.sk>
2022-11-10 09:24:40 +0100

    index2.php created; index.php changed

--- index.php
+++ index.php
@@ -4,7 +4,6 @@ echo '<h1>Daco 1</h1>';
 echo '<h1>Daco 2</h2>';
 echo '<h2>zas daco</h2>';
 echo '<p>dufam ze uz funguje</p>'
-
-
+echo '<a>https://phpiotmikulas.azurewebsites.net/index2.php</a>'
 
 ?>

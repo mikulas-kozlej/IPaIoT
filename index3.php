a1dca933ba0e0aa34d4451a8866ddf670ceb0a86
mikulas-kozlej <mikulas.kozlej@student.tuke.sk>
2022-11-10 09:56:25 +0100

    actuator.txt added index3 changed

--- index3.php
+++ index3.php
@@ -11,10 +11,12 @@
         fwrite($file1, $text1);
         fclose($file1);
         
+        /*
         $file2 = fopen("actuator.txt","w") or die("Unable to open file!");
         $text2 = "Value from actuator. Save this value to actuator.txt";
         fwrite($file2, $text2);
         fclose($file2);
+        */
         
         $file3 = fopen("actuator.txt","r") or die ("Subor neexistuje");
         $text3 = fread($file3,filesize("actuator.txt"));

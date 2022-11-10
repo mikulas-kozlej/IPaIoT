fdd76b2933333157ad4ed4a8ea28bb4868244259
mikulas-kozlej <mikulas.kozlej@student.tuke.sk>
2022-11-10 09:37:45 +0100

    link removed

--- index.php
+++ index.php
@@ -4,6 +4,5 @@ echo '<h1>Daco 1</h1>';
 echo '<h1>Daco 2</h2>';
 echo '<h2>zas daco</h2>';
 echo '<p>dufam ze uz funguje</p>'
-echo '<a href=https://phpiotmikulas.azurewebsites.net/index2.php>link index2.php</a>'
 
 ?>

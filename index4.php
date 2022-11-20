29287c6e6a052e5dbed9d4e4befb8f123501af34
mikulas-kozlej <mikulas.kozlej@student.tuke.sk>
2022-11-20 19:48:48 +0100

    var name fix

--- index4.php
+++ index4.php
@@ -1,10 +1,10 @@
 <?php
     $fname = $_GET["fname"];
     $lname = $_GET["lname"];
-    $lname = $_GET["date"];
-    $lname = $_GET["email"];
-    $lname = $_GET["fnum"];
-    $lname = $_GET["pnum"];
+    $date = $_GET["date"];
+    $email = $_GET["email"];
+    $fnum = $_GET["fnum"];
+    $pnum = $_GET["pnum"];
 
     $file = fopen("form_output", "w") or die("Unable to open file!");
     $text = $fname . "\n" . $lname . "\n" . $date . "\n" . $email . "\n" . $fnum . "\n" . $pnum;

5066bd385de257414b9cf3a6fafd3549d85d866f
mikulas-kozlej <mikulas.kozlej@student.tuke.sk>
2022-12-01 09:54:55 +0100

    index2 math changed

--- index2.php
+++ index2.php
@@ -5,10 +5,10 @@
         $sn2 = $_GET["b"];
 
         $text = "a=" . $sn1 . " b=" . $sn2;
-        $sum = $sn1 + $sn2;
+        $distsq = $sn1 * $sn1 + $sn2 *  $sn2;
 
         echo $text;
         echo "<br>";
-        echo "Sum:" . $sum;
+        echo "Distance squared:" . $distsq;
 
 ?>

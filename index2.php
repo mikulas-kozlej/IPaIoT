7f26a28bcec5d544ccd6b8c84433d72e10b6d524
mikulas-kozlej <114649979+mikulas-kozlej@users.noreply.github.com>
2022-12-21 22:16:12 +0100

    is_open added

--- index2.php
+++ index2.php
@@ -1,13 +1,12 @@
 <?php
     echo '<h1>getParameters page</h1>';
 
-        $sn1 = $_GET["temperature"];
-        $sn2 = $_GET["humidity"];
+        $temp = $_GET["temperature"];
+        $hum = $_GET["humidity"];
+        $is_open = $_GET["is_open";]
 
         $file = fopen("sensors.txt", "w") or die("Unable to open file!");
-        $text = $sn1 . ";" . $sn2;
+        $text = $temp . ";" . $hum .";" . $is_open; 
         fwrite($file, $text);
         fclose($file);
-        
-
 ?>

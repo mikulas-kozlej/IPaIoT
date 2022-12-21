647d3d20e20d8cceb810c37dbefafcf3769dc9ce
mikulas-kozlej <114649979+mikulas-kozlej@users.noreply.github.com>
2022-12-21 22:34:55 +0100

    underscore removed

--- index2.php
+++ index2.php
@@ -3,10 +3,10 @@
 
         $temp = $_GET["temperature"];
         $hum = $_GET["humidity"];
-        $is_open = $_GET["is_open";]
+        $isopen = $_GET["isopen";]
 
         $file = fopen("sensors.txt", "w") or die("Unable to open file!");
-        $text = $temp . ";" . $hum .";" . $is_open; 
+        $text = $temp . ";" . $hum .";" . $isopen; 
         fwrite($file, $text);
         fclose($file);
 ?>

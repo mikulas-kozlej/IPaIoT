c024e7c51892ec2ac86721d02703f9a5bac404c3
mikulas-kozlej <114649979+mikulas-kozlej@users.noreply.github.com>
2022-12-22 09:29:29 +0100

    fridge open test

--- index4.php
+++ index4.php
@@ -1,14 +1,8 @@
 <?php
-    $fname = $_GET["fname"];
-    $lname = $_GET["lname"];
-    $date = $_GET["date"];
-    $email = $_GET["email"];
-    $fnum = $_GET["fnum"];
-    $pnum = $_GET["pnum"];
+    $fridge = $_GET["fridge"];
 
     $file = fopen("form_output.txt", "w") or die("Unable to open file!");
-    $text = $fname . "\n" . $lname . "\n" . $date . "\n" . $email . "\n" . $fnum . "\n" . $pnum;
+    $text = $fridge;
     fwrite($file, $text);
     fclose($file);
-    echo 'Welcome ' . $fname . " " . $lname;
 ?>
